f9209ba66f1f2cbfff27dac88561a02cebb4f3d2
Tejas Gupta <73283098+Tejas320@users.noreply.github.com>
2024-09-04 22:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-pro')
 
 def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0], prompt])
